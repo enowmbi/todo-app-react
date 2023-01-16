0e2097bb5e2bf8353024ce5f26ecb3dac3fedbfd
Enow Mbi <benowmbi@yahoo.com>
2023-01-16 14:14:43 +0100

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { useState, useEffect } from "react"
+import Header from "./components/header"
 import './App.css';
 
 function App() {
     
     return (
         <div className="App">
-            <header className="App-header">
-                <div>
-                     <h1> TODOS </h1>
-                </div>
-            </header>
+        <Header/>
             <main className="App-main">
                 <h3>Put a new form here </h3>
             </main>
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () =>{
+
+    return(
+        <header className="App-header">
+            <div>
+            <h1> TODOS </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
