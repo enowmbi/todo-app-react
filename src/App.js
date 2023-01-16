270d49c0f7fd7dc664adc98ea8ba2d6b871f8038
Enow Mbi <benowmbi@yahoo.com>
2023-01-16 14:03:32 +0100

    Initial commit with React 18.2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react"
+import './App.css';
+
+function App() {
+    
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                     <h1> TODOS </h1>
+                </div>
+            </header>
+            <main className="App-main">
+                <h3>Put a new form here </h3>
+            </main>
+            <footer className="App-footer">
+                <h3>put footer here</h3>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
